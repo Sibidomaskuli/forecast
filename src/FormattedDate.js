abb7319d43867bc45a7700a5321a71e32e4eb958
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-08 13:08:14 +0100

    Adjusted App size

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -3,19 +3,18 @@ import React from "react";
 export default function FormattedDate(props) {
  console.log(props.date);
  let now = new Date();
- let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
- let today = days[now.getDay()];
  let date=now.getDate();
- let theDate = ("0" + date).slice(-2);
+ let thisDate = ("0" + date).slice(-2);
  let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
  let month = months[now.getMonth()];
- let theHours = now.getHours();
- theHours = ("0" + theHours).slice(-2);
- let theMinutes = now.getMinutes();
- theMinutes = ("0" + theMinutes).slice(-2);
+ let hours = now.getHours();
+ hours = ("0" + hours).slice(-2);
+ let minutes = now.getMinutes();
+ minutes = ("0" + minutes).slice(-2);
   return (
     <div>
-    {theDate} {month} {theHours}:{theMinutes}    
+    {thisDate} {month} {hours}:{minutes}    
    </div>  
   );
 }
+ 
--- src/FormattedWeekday.js
+++ src/FormattedWeekday.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function FormattedWeekday() {
  let now = new Date();
  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
- let today=days[now.getDay()];
+ let today = days[now.getDay()];
  return (
   <span>
    {today}
